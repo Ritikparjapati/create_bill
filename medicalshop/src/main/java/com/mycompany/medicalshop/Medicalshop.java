568c21ba5ec6f453f0c4a9ef3f66ec19bfd7f8f6
Ritik kumar <130444626+Ritikparjapati@users.noreply.github.com>
2023-06-16 15:57:42 +0530

    Add files via upload

---
+++ medicalshop/src/main/java/com/mycompany/medicalshop/Medicalshop.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.medicalshop;
+
+/**
+ *
+ * @author RITIK
+ */
+public class Medicalshop {
+    public static void main(String args[]){
+    new DownloadAPP().setVisible(true);
+    }
+}
